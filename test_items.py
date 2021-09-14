d1abd24e7cf5d116f99bf82163f2bfc59fc4c2fb
vitek501 <vitek.kurdyukov@gmail.com>
2021-09-14 22:45:41 +0500

    fixed assert

--- test_items.py
+++ test_items.py
@@ -1,11 +1,12 @@
 import time
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+
 def test_for_the_presence_buy_button(browser):
     browser.get(link)
-    assert browser.find_element_by_css_selector("button.btn-add-to-basket")
-    time.sleep(30)
-    
-
+    button = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert button != [], 'Button not found'
